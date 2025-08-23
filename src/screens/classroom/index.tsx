de4116056792a10b27ef7e2746bb5218bf8e706f
Marcia Rejane <marciarejane230@gmail.com>
2025-08-23 18:55:20 -0300

    first commit

--- src/screens/classroom/index.tsx
+++ src/screens/classroom/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container} from "./styles";
 import { Header } from "../../components/Header";
+import theme from "@theme/index";
 
 export  function Classroom() {
     return(
         <Container>
-            <Header title="Classroom" backButton={true}/>
+            <Header 
+             title="Classroom"
+             backButton={true} 
+             />
             
         </Container>
     );
